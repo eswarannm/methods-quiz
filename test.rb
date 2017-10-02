fd88eafd1206539c5ec5d5169771ef109bef21c2
Nithya Eswaran <eswarannm@s.dcsdk12.org>
2017-10-02 10:34:20 -0600

    tests for has_teen?

--- test.rb
+++ test.rb
@@ -3,3 +3,13 @@ require './methods_quiz'
 
 # TODO - write tests here
 
+describe 'methods_quiz' do 
+	describe 'has_teen?' do
+			it 'is a teen' do 
+				has_teen?(2,0,13).must_equal(true)
+			end
+			it 'is not teen' do 
+				has_teen?(2,0,3).must_equal(false)
+			end
+	end
+end
