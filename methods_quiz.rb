0c0310337f85872e464860450999cb083d6737f9
Nithya Eswaran <eswarannm@s.dcsdk12.org>
2017-10-02 10:39:09 -0600

    implement has_teen?

--- methods_quiz.rb
+++ methods_quiz.rb
@@ -1,5 +1,20 @@
   
 # TODO - write has_teen?
+def has_teen? (a,b,c)
+	if a == 13 || a == 14 || a == 15 || a == 16 || a == 17 || a == 18 || a == 19
+		return true
+	end
+
+	if b == 13 || b == 14 || b == 15 || b == 16 || b == 17 || b == 18 || b == 19
+		return true
+	end
+
+	if c == 13 || c == 14 || c == 15 || c == 16 || c == 17 || c == 18 || c == 19
+		return true
+	end
+
+	return false
+end
 
 # TODO - write not_string
 
