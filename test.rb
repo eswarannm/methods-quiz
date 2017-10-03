79a0f07dc53eff77a7ae48c9b373d4f65b4eea85
eswarannm <32105465+eswarannm@users.noreply.github.com>
2017-10-02 18:26:26 -0600

    fix icy_hot?

--- test.rb
+++ test.rb
@@ -14,24 +14,24 @@ describe 'methods_quiz' do
 			end
 	end
 
-	# describe 'not_string' do
-	# 		it 'no not' do 
-	# 			not_string("apple").must_equal(notapple)
-	# 		end
-	# 		it 'already has not' do 
-	# 			not_string("notapple").must_equal(notapple)
-	# 		end
-	# end
+	describe 'not_string' do
+	 		it 'no not' do 
+	 			not_string("apple").must_equal("notapple")
+	 		end
+	 		it 'already has not' do 
+	 			not_string("notapple").must_equal("notapple")
+	 		end
+	end
 
 	describe 'icy_hot?' do
 			it 'one icy' do 
-				icy_hot?(-1, 60).must_equal(true)
+				icy_hot?(-1, 60).must_equal(false)
 			end
 			it 'one hot' do 
-				icy_hot?(40, 101).must_equal(true)
+				icy_hot?(40, 101).must_equal(false)
 			end
 			it 'both icy hot' do 
-				icy_hot?(-1, 103).must_equal(false)
+				icy_hot?(-1, 103).must_equal(true)
 			end
 			it 'not icy hot' do 
 				icy_hot?(40, 90).must_equal(false)
